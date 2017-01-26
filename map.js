efce3eab12a0d778a7125b7bcf598f1a72248451
Andres Colonia <andreskal@gmail.com>
2017-01-26 11:24:20 -0300

    add init google maps

--- map.js
+++ map.js
@@ -1,22 +1,14 @@
-function BaseMap(){
-  this.options = {};
-  this.markers = [];
-  this.polylines = [];
-  this.circles = [];
-  this.distance = 0;
-  this.options.div = options.div || "#map";
-  this.options.zoom = options.zoom || 8;
-  this.options.lat = options.lat || 0;
-  this.options.lng = options.lng || 0;
-  this.options.styles = options.styles || false;
-  this.options.mapTypeControl = options.mapTypeControl || false;
-
-  this.mapOptions = {
-    zoom: this.options.zoom,
-    center: new google.maps.LatLng(this.options.lat, this.options.lng),
-    styles: this.options.styles,
-    mapTypeControl: this.options.mapTypeControl
-  };
-
-  this.map = new google.maps.Map(getElementById(this.options.div), this.mapOptions);
-}
+var BaseMap = {
+  init: function() {
+    var _this = this;
+    _this.google_maps();
+    _this.initialize();
+  },
+  google_maps: function(){
+    this.mapOptions = {
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(document.getElementById("map"), this.mapOptions);
+  },
+};
