e2b68fdd1b32d2a6145b3f34e2f0063d26e4c882
Andres Colonia <andreskal@gmail.com>
2017-01-26 11:25:15 -0300

    add markers and test new position with one meter

--- map.js
+++ map.js
@@ -11,4 +11,28 @@ var BaseMap = {
     };
     this.map = new google.maps.Map(document.getElementById("map"), this.mapOptions);
   },
+  initialize: function() {
+    var map = this.map;
+    var position1 = new google.maps.LatLng(-33.50694, -70.72127);
+    var position2 = new google.maps.LatLng(-33.50684, -70.72111);
+    var bounds = new google.maps.LatLngBounds();
+    bounds.extend(position1);
+    bounds.extend(position2);
+    map.fitBounds(bounds);
+    var marker1 = new google.maps.Marker({
+      position: position1,
+      map: map,
+      label: "Marker 1"
+    });
+    var marker2 = new google.maps.Marker({
+      position: position2,
+      map: map,
+      label: "Marker 2"
+    });
+    var marker1meter = new google.maps.Marker({
+      position: new google.maps.geometry.spherical.computeOffset(position1, 1, google.maps.geometry.spherical.computeHeading(position1, position2)),
+      map: map,
+      label: "new position"
+    })
+  }
 };
